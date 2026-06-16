308b71b44778501600ed2eb7a1101f228aa93016
leouft <leonardo.soares1@mail.uft.edu.br>
2026-06-16 15:51:40 -0300

    Copy list

---
+++ 1-LinkedList/EX/4-CopyList/TLList.c
@@ -0,0 +1,116 @@
+#include "TLList.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _no {
+    int info;
+    struct _no *prox;
+} TNo;
+
+struct _list{
+    TNo* inicio;
+};
+
+TLList* TLList_create() {
+    TLList* new = malloc(sizeof(TLList));
+    if (!new) return NULL;
+    new->inicio = NULL;
+    return new;
+}
+
+TNo* TNo_createNFill(int info) {
+    TNo* new = malloc(sizeof(TNo));
+    if (!new) return NULL;
+    new->info = info;
+    new->prox = NULL;
+    return new;
+}
+
+bool TLList_insert_begin(TLList* lista, int info) {
+    TNo* new = TNo_createNFill(info);
+    if (!new) return false;
+    if (lista->inicio != NULL) {
+        new->prox = lista->inicio;
+    }
+    lista->inicio = new;
+    return true;
+}
+
+bool TLList_insert_end(TLList* lista, int info) {
+    TNo* new = TNo_createNFill(info);
+    if (!new) return false;
+    TNo* aux = lista->inicio;
+    if (lista->inicio == NULL) {
+        lista->inicio = new;
+    }
+    else {
+        while(aux->prox != NULL){
+            aux = aux->prox;
+        }
+        aux->prox = new;
+    }
+    return true;
+}
+
+bool TLList_remove_end(TLList* lista, int* pinfo) {
+    if (lista->inicio != NULL) {
+        TNo** aux = &lista->inicio;
+        while ((*aux)->prox != NULL) {
+            aux = &(*aux)->prox;
+        }
+        *pinfo = (*aux)->info;
+        free((*aux));
+        (*aux) = NULL;
+        return true;
+    }
+    return false;
+}
+
+bool TLList_remove_begin(TLList* lista, int* pinfo) {
+    if (lista->inicio != NULL) {
+        TNo* aux = lista->inicio;
+        *pinfo = aux->info;
+        lista->inicio = aux->prox;
+        free(aux);
+        return true;
+    }
+    return false;
+}
+
+void TLList_print(TLList* lista) {
+    if (lista->inicio != NULL) {
+        TNo* aux = lista->inicio;
+        while (aux != NULL) {
+            printf("%d -> ", aux->info);
+            aux = aux->prox;
+        }
+        printf("\n");
+    }
+    else
+        printf("A lista está vazia.\n");
+}
+
+TLList* TLList_copy(TLList* lista) {
+    TLList* new = TLList_create();
+    if (!new) return NULL;
+    TNo* aux = lista->inicio;
+    while (aux) {
+        TNo* aux2 = new->inicio;
+        bool foundRepeated = false;
+        while (aux2) {
+            if (aux2->info == aux->info) {
+                foundRepeated = true;
+                break;
+            }
+            aux2 = aux2->prox;
+        }
+
+        if (!foundRepeated) {
+            TLList_insert_end(new, aux->info);
+        }
+
+        aux = aux->prox;
+    }
+    return new;
+}
